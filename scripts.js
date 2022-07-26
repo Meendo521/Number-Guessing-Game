ed3bfaa6662c66f21994a467883d7b37e6a8670e
Meen Philip <meenphilip@gmail.com>
2022-07-26 17:05:35 +0300

    Declare games values,UI elements,add eventlistener, validate and handle win case

--- scripts.js
+++ scripts.js
@@ -6,3 +6,48 @@ GAME FUNCTION
 - Notify the player of the correct answer if loose
 - Let player choose to play again
 */
+
+//Game values
+let min = 1;
+let max = 10;
+let winningNumber = 2;
+let guessLeft = 3;
+
+//UI Elements
+const gameEl = document.querySelector("#game");
+const minNumEl = document.querySelector(".min-num");
+const maxNumEl = document.querySelector(".max-num");
+const guessInputEl = document.querySelector("#guess-input");
+const guessBtnEl = document.querySelector("#guess-btn");
+const message = document.querySelector(".message");
+
+//Ass UI min & max
+minNumEl.textContent = min;
+maxNumEl.textContent = max;
+
+//create EventListener on Button
+guessBtnEl.addEventListener("click", () => {
+  let guess = parseInt(guessInputEl.value);
+  // console.log(guess);
+
+  //Validate
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, "red");
+  }
+
+  //check if won
+  if (guess === winningNumber) {
+    //Disable input
+    guessInputEl.disabled = true;
+    //change border color to green
+    guessInputEl.style.borderColor = "green";
+    //win case
+    setMessage(`${winningNumber} is correct, YOU WIN!!`, "green");
+  } else {
+  }
+});
+
+const setMessage = (msg, color) => {
+  message.style.color = color;
+  message.textContent = msg;
+};
