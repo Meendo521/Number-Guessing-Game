ef42055b77a82828e574745c0cfbb7b0cd37c09e
Meen Philip <meenphilip@gmail.com>
2022-07-26 16:20:07 +0300

    add games rules to in js scripts

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-
+/*
+GAME FUNCTION
+- Player must guess a number between a min and max
+- Players get a certain amount remaining
+- Notify player of numbers of guesses left
+- Notify the player of the correct answer if loose
+- Let player choose to play again
+*/
